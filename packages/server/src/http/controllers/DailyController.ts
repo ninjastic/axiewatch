41b5f109347d82162171d94c4b266b3b12f294ec
Ninjastic <ninjasticdev@protonmail.com>
2021-10-21 18:18:01 -0300

    fix: daily controller

--- packages/server/src/http/controllers/DailyController.ts
+++ packages/server/src/http/controllers/DailyController.ts
@@ -1,54 +1,12 @@
 import { Request, Response } from 'express';
 
-import dayjs from '@src/services/dayjs';
-import { cache } from '@src/services/cache';
 import { supabase } from '@src/services/supabase';
 import Sync from '@src/models/Sync';
-import Tracking from '@src/models/Tracking';
-
-async function getDaily(address: string, authorization: string) {
-  const cacheKey = `@daily:${address}:days`;
-  const cacheTime = 1000 * 60 * 5; // 5 minutes
-
-  let isTracking: boolean | null = null;
-
-  const { user } = await supabase.auth.api.getUser(authorization);
-
-  if (user) {
-    isTracking = !!(await Sync.query()
-      .select('id')
-      .where('user_id', user.id)
-      .then(response => response));
-  }
-
-  const cached = await cache.get(cacheKey);
-  if (cached) return { ...JSON.parse(cached), isTracking };
-
-  const days = await Tracking.query()
-    .select('slpAmount', 'createdAt')
-    .where('address', address)
-    .andWhere('createdAt', '>=', dayjs.utc().subtract(14, 'day').startOf('day').toDate())
-    .orderBy('createdAt', 'DESC');
-
-  const daysFormatted =
-    days
-      .map(entry => ({
-        day: dayjs.utc(entry.createdAt).format('YYYY-MM-DD'),
-        totalSlp: entry.slpAmount,
-      }))
-      .reverse() ?? [];
-
-  const data = {
-    dates: daysFormatted,
-  };
-
-  if (data) cache.set(cacheKey, JSON.stringify(data), 'PX', cacheTime);
-  return { ...data, isTracking };
-}
+import { getScholarHistoricalSlp } from '../services/getScholarHistoricalSlp';
 
 export class DailyController {
   async get(req: Request, res: Response): Promise<Response> {
-    const { address } = req.query;
+    const { address } = req.query as { address: string };
     const authorization = req.get('authorization');
 
     if (!address) {
@@ -59,7 +17,23 @@ export class DailyController {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const data = await getDaily(String(address), String(authorization));
+    let isTracking: boolean | null = null;
+
+    const { user } = await supabase.auth.api.getUser(authorization ?? '');
+
+    if (user) {
+      isTracking = !!(await Sync.query()
+        .select('id')
+        .where('user_id', user.id)
+        .then(response => response));
+    }
+
+    const { dates } = await getScholarHistoricalSlp(address);
+
+    const data = {
+      dates,
+      isTracking,
+    };
 
     return res.json(data);
   }
