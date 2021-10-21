23633aacfba7f79ec5bc9791b1d062add0e8b12d
Ninjastic <ninjasticdev@protonmail.com>
2021-10-21 17:18:31 -0300

    chore: update objection

--- packages/server/src/services/database.ts
+++ packages/server/src/services/database.ts
@@ -6,6 +6,7 @@ export const database = Knex({
   connection: {
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
+    port: Number(process.env.DATABASE_PORT || 5432),
     password: process.env.DATABASE_PASSWORD,
     db: process.env.DATABASE_NAME,
   },
