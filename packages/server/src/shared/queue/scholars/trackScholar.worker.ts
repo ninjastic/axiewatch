482437fe0cee4c23b81e6617e4abddbf1b6c0b57
Ninjastic <ninjasticdev@protonmail.com>
2021-10-28 04:15:46 -0300

    fix: trackScholarsQueue name

--- packages/server/src/shared/queue/scholars/trackScholar.worker.ts
+++ packages/server/src/shared/queue/scholars/trackScholar.worker.ts
@@ -3,7 +3,7 @@ import { Job, Worker } from 'bullmq';
 import redis from '@src/config/redis';
 import { trackScholarJob } from './trackScholar.job';
 
-const worker = new Worker('scholarsQueue', async (job: Job) => trackScholarJob(job.data.address), {
+const worker = new Worker('trackScholarQueue', async (job: Job) => trackScholarJob(job.data.address), {
   limiter: {
     max: Number(process.env.TRACK_SCHOLARS_QUEUE_LIMITER),
     duration: Number(process.env.TRACK_SCHOLARS_QUEUE_DURATION_MS),
