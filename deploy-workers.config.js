e029db7b01e754b068edfe9381895720d096ba1b
Ninjastic <ninjasticdev@protonmail.com>
2021-10-29 16:50:50 -0300

    wip

--- deploy-workers.config.js
+++ deploy-workers.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '/root/.ssh/ninja',
       user: 'ninja',
       host: ['23.94.3.119'], // '45.129.228.184', '45.129.228.183', '88.218.200.87', '88.218.200.85'
-      ref: 'origin/master',
+      ref: 'origin/chore-deploy-workers',
       repo: 'git@github.com:ninjastic/axiewatch.git',
       path: '/home/ninja/axiewatch',
       'post-deploy': 'yarn install && yarn server build && pm2 startOrRestart workers.ecosystem.config.js',
