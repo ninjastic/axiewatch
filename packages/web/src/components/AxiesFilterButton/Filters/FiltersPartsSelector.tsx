66e27bfda89bb93af197f92aa7f36f468a3ed4b1
Ninjastic <ninjasticdev@protonmail.com>
2021-12-06 01:49:45 -0300

    feat: axie filters parts icons

--- packages/web/src/components/AxiesFilterButton/Filters/FiltersPartsSelector.tsx
+++ packages/web/src/components/AxiesFilterButton/Filters/FiltersPartsSelector.tsx
@@ -1,9 +1,11 @@
-import { FormControl, FormLabel } from '@chakra-ui/react';
+import { FormControl, FormLabel, HStack, Text } from '@chakra-ui/react';
 import { Dispatch, SetStateAction } from 'react';
 import { useRecoilValue } from 'recoil';
 import { Select } from 'chakra-react-select';
 
-import { axiePartsAtom, ScholarAxiesFilter } from '../../../recoil/scholars';
+import bodyParts from '../../../constants/body-parts.json';
+import { AxieClass, axiePartsAtom, ScholarAxiesFilter } from '../../../recoil/scholars';
+import { AxiePartIcon, AxiePartIconType } from '@src/components/Icons/AxiePartIcon';
 
 interface FilterProps {
   setFilter: Dispatch<SetStateAction<ScholarAxiesFilter>>;
@@ -13,15 +15,43 @@ interface FilterProps {
 export const FiltersPartsSelector = ({ setFilter, formValues }: FilterProps): JSX.Element => {
   const parts = useRecoilValue(axiePartsAtom);
 
-  const defaultValue = formValues.parts.map(part => ({
-    label: part.replace(/\w+-/i, '').replace(/-/g, ' '),
-    value: part,
-  }));
+  const defaultValue = formValues.parts.map(part => {
+    const bodyPart = bodyParts.find(bp => bp.partId === part);
 
-  const options = parts.map(part => ({
-    label: part.replace(/\w+-/i, '').replace(/-/g, ' '),
-    value: part,
-  }));
+    return {
+      label: (
+        <HStack>
+          <AxiePartIcon
+            type={bodyPart.type as AxiePartIconType}
+            bg={bodyPart.class as AxieClass}
+            borderRadius="sm"
+            fontSize="lg"
+          />
+          <Text>{part.replace(/\w+-/i, '').replace(/-/g, ' ')}</Text>
+        </HStack>
+      ),
+      value: part,
+    };
+  });
+
+  const options = parts.map(part => {
+    const bodyPart = bodyParts.find(bp => bp.partId === part);
+
+    return {
+      label: (
+        <HStack>
+          <AxiePartIcon
+            type={bodyPart.type as AxiePartIconType}
+            bg={bodyPart.class as AxieClass}
+            borderRadius="sm"
+            fontSize="lg"
+          />
+          <Text>{part.replace(/\w+-/i, '').replace(/-/g, ' ')}</Text>
+        </HStack>
+      ),
+      value: part,
+    };
+  });
 
   function handleChange(selected: string[]) {
     setFilter(old => ({
