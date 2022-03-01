4a9288a85b8cdb1a7daf43790d30e96ad3ca07b2
Ninjastic <ninjasticdev@protonmail.com>
2022-03-01 12:18:26 +0000

    feat: testing new pvp endpoint

--- packages/server/src/shared/http/services/getScholarPvp.ts
+++ packages/server/src/shared/http/services/getScholarPvp.ts
@@ -10,14 +10,14 @@ interface APIOldScholarPvpResponse {
   }>;
 }
 
-// interface APIScholarPvpResponse {
-//   _items: Array<{
-//     clientID: string;
-//     elo: number;
-//     rank: number;
-//     name: string;
-//   }>;
-// }
+interface APIScholarPvpResponse {
+  _items: Array<{
+    clientID: string;
+    elo: number;
+    rank: number;
+    name: string;
+  }>;
+}
 
 export interface ScholarPvpData {
   name: string;
@@ -29,12 +29,12 @@ export const getScholarPvp = async (address: string): Promise<ScholarPvpData> =>
   const cacheKey = `v1:scholarPvp:${address}`;
   const cacheTime = 1000 * 60 * 15; // 15 minutes
 
-  // const apiUrl = 'https://game-api-pre.skymavis.com/v1/leaderboards';
-  // const apiParams = {
-  //   clientID: address,
-  //   offset: 0,
-  //   limit: 0,
-  // };
+  const apiUrl = 'https://game-api-pre.skymavis.com/v1/leaderboards';
+  const apiParams = {
+    clientID: address,
+    offset: 0,
+    limit: 0,
+  };
 
   const apiBackupUrl = 'https://game-api.skymavis.com/game-api/leaderboard';
   const apiBackupParams = {
@@ -46,35 +46,35 @@ export const getScholarPvp = async (address: string): Promise<ScholarPvpData> =>
   const cached = await cache.get(cacheKey);
   if (cached) return JSON.parse(cached);
 
-  // const data = await proxiedApi
-  //   .get<APIScholarPvpResponse>(apiUrl, { params: apiParams })
-  //   .then(response => response.data._items.find(item => item.clientID.toLowerCase() === address.toLowerCase()));
+  const data = await proxiedApi
+    .get<APIScholarPvpResponse>(apiUrl, { params: apiParams })
+    .then(response => response.data._items.find(item => item.clientID.toLowerCase() === address.toLowerCase()));
 
-  // if (!data) {
-  const dataBackup = await proxiedApi
-    .get<APIOldScholarPvpResponse>(apiBackupUrl, { params: apiBackupParams })
-    .then(response => response.data.items.find(item => item.client_id.toLowerCase() === address.toLowerCase()));
+  if (!data) {
+    const dataBackup = await proxiedApi
+      .get<APIOldScholarPvpResponse>(apiBackupUrl, { params: apiBackupParams })
+      .then(response => response.data.items.find(item => item.client_id.toLowerCase() === address.toLowerCase()));
 
-  if (!dataBackup) {
-    throw new Error('Something went wrong');
+    if (!dataBackup) {
+      throw new Error('Something went wrong');
+    }
+
+    const scholarPvp = {
+      name: dataBackup.name,
+      elo: dataBackup.elo,
+      rank: dataBackup.rank,
+    };
+
+    await cache.set(cacheKey, JSON.stringify(scholarPvp), 'PX', cacheTime);
+    return scholarPvp;
   }
 
   const scholarPvp = {
-    name: dataBackup.name,
-    elo: dataBackup.elo,
-    rank: dataBackup.rank,
+    name: data.name,
+    elo: data.elo,
+    rank: data.rank,
   };
 
   await cache.set(cacheKey, JSON.stringify(scholarPvp), 'PX', cacheTime);
   return scholarPvp;
-  // }
-
-  // const scholarPvp = {
-  //   name: data.name,
-  //   elo: data.elo,
-  //   rank: data.rank,
-  // };
-
-  // await cache.set(cacheKey, JSON.stringify(scholarPvp), 'PX', cacheTime);
-  // return scholarPvp;
 };
