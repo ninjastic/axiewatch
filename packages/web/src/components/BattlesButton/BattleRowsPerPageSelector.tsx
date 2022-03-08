873173b366459640da62f437ef380bbc8d53ba14
Ninjastic <ninjasticdev@protonmail.com>
2022-03-08 09:51:58 +0200

    feat: more battles perPage options

--- packages/web/src/components/BattlesButton/BattleRowsPerPageSelector.tsx
+++ packages/web/src/components/BattlesButton/BattleRowsPerPageSelector.tsx
@@ -15,8 +15,11 @@ export const BattleRowsPerPageSelector = ({ value, onChange }: BattleRowsPerPage
 
       <MenuList>
         <MenuItem onClick={() => onChange(10)}>10</MenuItem>
+        <MenuItem onClick={() => onChange(10)}>20</MenuItem>
         <MenuItem onClick={() => onChange(25)}>25</MenuItem>
+        <MenuItem onClick={() => onChange(25)}>40</MenuItem>
         <MenuItem onClick={() => onChange(50)}>50</MenuItem>
+        <MenuItem onClick={() => onChange(50)}>60</MenuItem>
       </MenuList>
     </Menu>
   );
